eceeb9a385140a7442d8b39ea1658b4fbf4ea16a
Patrick Larocque <patricklarocque1@gmail.com>
2024-12-07 18:50:35 -0500

    day02, part02 solved

--- day02/solutions.go
+++ day02/solutions.go
@@ -35,19 +35,7 @@ func solvePart1() int {
 			nums[i] = num
 		}
 
-		// True = descending, false = ascending
-		descending := nums[0] > nums[1]
-		// Flip to false if we ever fail the satisfiedSafeCondition for given subarray
-		safe := true
-
-		// verify each []int subarray against 'safe' condition
-		for i := range len(nums) - 1 {
-			diff := nums[i] - nums[i+1]
-			if !satisfiesSafeConditions(diff, descending) {
-				safe = false
-			}
-		}
-		if safe {
+		if isArraySafe(nums) {
 			count++
 		}
 	}
@@ -55,7 +43,31 @@ func solvePart1() int {
 }
 
 func solvePart2() int {
-	return 0
+	count := 0
+	file, err := os.Open("day02/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		strs := strings.Fields(scanner.Text())
+		nums := make([]int, len(strs))
+
+		// Convert strings to ints
+		for i := range strs {
+			num, err := strconv.Atoi(strs[i])
+			if err != nil {
+				panic(err)
+			}
+			nums[i] = num
+		}
+
+		if isArraySafeWithProblemDampener(nums) {
+			count++
+		}
+	}
+	return count
 }
 
 // Helpers
@@ -80,4 +92,47 @@ func getAbsoluteDifference(val int) int {
 		return -val
 	}
 	return val
+}
+
+func isArraySafeWithProblemDampener(nums []int) bool {
+	// First check if the array is safe without removing any elements
+	if isArraySafe(nums) {
+		return true
+	}
+
+	// Try removing each element one at a time
+	for i := range nums {
+		// Create a new array without the current element
+		dampened := make([]int, 0, len(nums)-1)
+		dampened = append(dampened, nums[:i]...)
+		dampened = append(dampened, nums[i+1:]...)
+
+		// Check if the dampened array is safe
+		if isArraySafe(dampened) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isArraySafe(nums []int) bool {
+	if len(nums) < 2 {
+		return true
+	}
+
+	// Determine initial direction based on first two elements
+	descending := nums[0] > nums[1]
+
+	// Check each pair of adjacent numbers
+	for i := 0; i < len(nums)-1; i++ {
+		diff := nums[i] - nums[i+1]
+
+		// Check both direction consistency and difference magnitude
+		if !satisfiesSafeConditions(diff, descending) {
+			return false
+		}
+	}
+
+	return true
 }
