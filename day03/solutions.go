0f632511f1478b9af7dc3d78e98c379f5798520c
Patrick Larocque <patricklarocque1@gmail.com>
2024-12-08 10:47:50 -0500

    day03, part02 solved, day04 setup

--- day03/solutions.go
+++ day03/solutions.go
@@ -39,5 +39,52 @@ func solvePart1() int {
 }
 
 func solvePart2() int {
-	return 0
+	var sum int
+	file, err := os.Open("day03/input.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	mulExpression := regexp.MustCompile(`^mul\(([1-9]\d{0,2}),([1-9]\d{0,2})\)`)
+	doExpression := regexp.MustCompile(`^do\(\)`)
+	dontExpression := regexp.MustCompile(`^don't\(\)`)
+	enabled := true
+
+	for scanner.Scan() {
+		i := 0
+
+		for i < len(scanner.Text()) {
+			currentSlice := scanner.Text()[i:]
+
+			if mul := mulExpression.FindStringSubmatch(currentSlice); mul != nil {
+				if enabled {
+					num1, err1 := strconv.Atoi(mul[1])
+					num2, err2 := strconv.Atoi(mul[2])
+
+					if err1 != nil || err2 != nil {
+						panic("An error occurred")
+					}
+					i += len(mul[0])
+					sum += num1 * num2
+					continue
+				}
+			}
+
+			if dont := dontExpression.FindString(currentSlice); dont != "" {
+				enabled = false
+				i += len(dont)
+				continue
+			}
+
+			if do := doExpression.FindString(currentSlice); do != "" {
+				enabled = true
+				i += len(do)
+				continue
+			}
+
+			i++
+		}
+	}
+
+	return sum
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"advent/day01"
 	"advent/day02"
 	"advent/day03"
+	"advent/day04"
 	"fmt"
 )
 
@@ -16,4 +17,7 @@ func main() {
 
 	day3Part1, day3Part2 := day03.Solve()
 	fmt.Printf("Day 3:\n  Part 1: %v\n  Part 2: %v\n", day3Part1, day3Part2)
+
+	day4Part1, day4Part2 := day04.Solve()
+	fmt.Printf("Day 4:\n  Part 1: %v\n  Part 2: %v\n", day4Part1, day4Part2)
 }
